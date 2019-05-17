741fc7e37268701a3805819d383d2ac36f38a8a7
i0brendan0 <i0brendan0@github.com>
2019-05-17 02:26:20 -0500

    fix tm swap

--- data/items/marts.asm
+++ data/items/marts.asm
@@ -152,7 +152,7 @@ MartGoldenrod5F3:
 	db TM_THUNDERPUNCH
 	db TM_FIRE_PUNCH
 	db TM_ICE_PUNCH
-	db TM_ROCK_SMASH
+	db TM_FLASH
 	db -1 ; end
 
 MartGoldenrod5F4:
@@ -161,7 +161,7 @@ MartGoldenrod5F4:
 	db TM_FIRE_PUNCH
 	db TM_ICE_PUNCH
 	db TM_HEADBUTT
-	db TM_ROCK_SMASH
+	db TM_FLASH
 	db -1 ; end
 
 MartOlivine:
