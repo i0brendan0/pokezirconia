9b6055a7a4b0fd47392d2a0c4ab362ef80122dc7
i0brendan0 <i0brendan0@github.com>
2019-06-22 11:52:16 -0500

    Update weather_images.asm

--- gfx/weather_images.asm
+++ gfx/weather_images.asm
@@ -1,13 +1,13 @@
 WeatherImages::
 
 RainWeatherImage:
-INCLUDE "gfx/battle/weather/rain.2bpp"
+INCBIN "gfx/battle/weather/rain.2bpp"
 
 SunWeatherImage:
-INCLUDE "gfx/battle/weather/sun.2bpp"
+INCBIN "gfx/battle/weather/sun.2bpp"
 
 SandstormWeatherImage:
-INCLUDE "gfx/battle/weather/sand.2bpp"
+INCBIN "gfx/battle/weather/sand.2bpp"
 
 HailWeatherImage:
-INCLUDE "gfx/battle/weather/hail.2bpp"
+INCBIN "gfx/battle/weather/hail.2bpp"
